e35961c11893746099cfc4a3b5c837f137b26a91
chand <chand@PC>
2025-08-07 21:30:12 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/agreenspace',
+  assetPrefix: '/agreenspace/',
 };
 
 export default nextConfig;
